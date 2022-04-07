659b807fa2bae42f95f5af5a3394a1e42bb364c5
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-04-07 22:20:18 +0100

    test: making sure the userService throws InvalidParameterException when validation error occurs and throws exceptoin when encrypt throws.

--- business/src/test/java/com/shareit/service/UserServiceTest.java
+++ business/src/test/java/com/shareit/service/UserServiceTest.java
@@ -2,26 +2,38 @@ package com.shareit.service;
 
 import com.shareit.data.repository.UserRepository;
 import com.shareit.domain.User;
+import com.shareit.domain.dto.CreateUser;
+import com.shareit.exception.InvalidParameterException;
+import com.shareit.infrastructure.cryptography.Encrypter;
 import com.shareit.utils.EmailValidator;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 class UserServiceTest {
 
     private final UserService userService;
 
-    @Mock
-    private UserRepository userRepository;
-    @Mock
-    private EmailValidator emailValidator;
+    private final UserRepository userRepository;
+    private final EmailValidator emailValidator;
+    private final Encrypter encrypter;
+
+    private final CreateUser createUser = new CreateUser(
+            "any_email@mail.com",
+            "any_password",
+            "any_password",
+            "any_name",
+            LocalDate.now());
 
     private List<User> users = Collections.singletonList(
             new User(1L,
@@ -31,7 +43,11 @@ class UserServiceTest {
                     LocalDate.now()));
 
     public UserServiceTest() {
-        this.userService = new UserService(userRepository, emailValidator);
+        userRepository = Mockito.mock(UserRepository.class);
+        emailValidator = Mockito.mock(EmailValidator.class);
+        encrypter = Mockito.mock(Encrypter.class);
+
+        this.userService = new UserService(userRepository, emailValidator, encrypter);
     }
 
     @Test
@@ -45,6 +61,47 @@ class UserServiceTest {
         assertEquals(allUsers.get(0), users.get(0));
     }
 
+    @Test
+    public void testCreateUserWhenInvalidEmailProvided() {
+        when(emailValidator.isValid(anyString())).thenReturn(false);
+        InvalidParameterException invalidParameterException = assertThrows(InvalidParameterException.class, () -> userService.createUser(createUser));
+        assertEquals("email", invalidParameterException.getParamName());
+    }
 
+    @Test
+    public void testCreateUserWithWrongPasswordConfirmation() {
+        when(emailValidator.isValid(anyString())).thenReturn(true);
+        when(userRepository.save(any(User.class))).thenReturn(users.get(0));
+        createUser.setPasswordConfirmation("wrong_password");
+
+        InvalidParameterException invalidParameterException = assertThrows(InvalidParameterException.class, () -> userService.createUser(createUser));
+        assertEquals("passwordConfirmation", invalidParameterException.getParamName());
+    }
+
+    @Test
+    public void testCreateUserWhenEncrypterThrowsException() {
+        when(emailValidator.isValid(anyString())).thenReturn(true);
+        when(encrypter.encrypt(anyString())).thenThrow(RuntimeException.class);
+        when(userRepository.save(any(User.class))).thenReturn(users.get(0));
+
+        assertThrows(Exception.class, () -> userService.createUser(createUser));
+    }
+
+    @Test
+    public void testCreateUser() {
+        when(emailValidator.isValid(anyString())).thenReturn(true);
+        when(encrypter.encrypt(anyString())).thenReturn("encrypted_password");
+        when(userRepository.save(any(User.class))).thenReturn(users.get(0));
+
+
+        Long userId = userService.createUser(createUser);
+
+        verify(userRepository).save(
+                new User(createUser.getEmail(),
+                "encrypted_password",
+                        createUser.getName(),
+                        createUser.getBirthDate()));
+        assertEquals(1L, userId);
+    }
 
 }
