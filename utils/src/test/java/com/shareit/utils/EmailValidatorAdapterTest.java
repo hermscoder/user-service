e7249f6316b5d567e68088969d7f9ae13ffb4fa9
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-04-07 08:25:50 +0100

    test: making sure EmailValidatorAdapter returns true when EmailValidator returns true.

--- utils/src/test/java/com/shareit/utils/EmailValidatorAdapterTest.java
+++ utils/src/test/java/com/shareit/utils/EmailValidatorAdapterTest.java
@@ -1,18 +1,32 @@
 package com.shareit.utils;
 
+import org.apache.commons.validator.routines.EmailValidator;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmailValidatorAdapterTest {
 
     private final EmailValidatorAdapter emailValidatorAdapter;
+    private EmailValidator emailValidator;
 
     public EmailValidatorAdapterTest() {
-        this.emailValidatorAdapter = new EmailValidatorAdapter();
+        emailValidator = Mockito.mock(EmailValidator.class);
+        this.emailValidatorAdapter = new EmailValidatorAdapter(emailValidator);
     }
+
     @Test
     public void testIsValid() {
+        Mockito.when(emailValidator.isValid(ArgumentMatchers.any())).thenReturn(true);
+        boolean emailValid = emailValidatorAdapter.isValid("any_email@mail.com");
+        assertTrue(emailValid);
+    }
+
+    @Test
+    public void testIsNotValid() {
+        Mockito.when(emailValidator.isValid(ArgumentMatchers.any())).thenReturn(false);
         boolean emailValid = emailValidatorAdapter.isValid("any_email@mail.com");
         assertFalse(emailValid);
     }
