9887e13e7a0864d0cd4a8f9c62ccada88da708cf
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-04-07 07:44:14 +0100

    test: make sure user registration returns OK

--- core/src/test/java/com/shareit/core/UserControllerWebTest.java
+++ core/src/test/java/com/shareit/core/UserControllerWebTest.java
@@ -1,33 +1,85 @@
 package com.shareit.core;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.shareit.domain.dto.CreateUser;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class UserControllerWebTest {
 
+    private static final String USER_ENDPOINT = "/v1/user";
     @Autowired
     MockMvc mockMvc;
 
     @Test
     public void testGetUsers() throws Exception {
-        mockMvc.perform(get("/v1/user"))
+        mockMvc.perform(get(USER_ENDPOINT))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].id").value("1"))
                 .andExpect(jsonPath("$[0].birthDate").value(LocalDate.now().toString()))
                 .andExpect(jsonPath("$[0].email").value("test@gmail.com"))
                 .andExpect(jsonPath("$[0].name").value("test"))
                 .andExpect(jsonPath("$[0].password").value("password"));
+    }
+
+    @Test
+    public void testUserRegisterOk() throws Exception {
+        CreateUser createUser = new CreateUser(
+                "any_email@mail.com",
+                "any_password",
+                "any_password",
+                "any_name",
+                LocalDate.now());
+
+        mockMvc.perform(
+                post(USER_ENDPOINT)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(createUser)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andReturn();
+    }
+
+    @Test
+    public void testUserRegisterEmailNotValid() throws Exception {
+        CreateUser createUser = new CreateUser(
+                "invalid_email@mail.com",
+                "any_password",
+                "any_password",
+                "any_name",
+                LocalDate.now());
+
+        mockMvc.perform(
+                post(USER_ENDPOINT)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(createUser)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andReturn();
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- presentation/src/test/java/com/shareit/presentation/UserControllerTest.java
+++ presentation/src/test/java/com/shareit/presentation/UserControllerTest.java
@@ -1,18 +1,56 @@
 package com.shareit.presentation;
 
+import com.shareit.domain.User;
+import com.shareit.domain.dto.CreateUser;
+import com.shareit.domain.dto.UserCreated;
 import com.shareit.service.UserService;
-import org.mockito.Mock;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-class UserControllerTest {
+
+public class UserControllerTest {
 
     private final UserController userController;
+    private final UserService userService;
 
-    @Mock
-    private UserService userService;
+    private final List<User> userList;
 
     public UserControllerTest() {
+        userService = Mockito.mock(UserService.class);
         userController = new UserController(userService);
+        User expected = new User(1L, "test@gmail.com", "password", "test", LocalDate.now());
+        userList = Collections.singletonList(expected);
+    }
+
+    @Test
+    public void testGetUsers() {
+
+        when(userService.findAll()).thenReturn(userList);
+        List<User> users = userController.getUsers();
+        assertNotNull(users);
+        assertEquals(userList.size(), users.size());
+        assertEquals(userList.get(0), users.get(0));
+    }
+
+    @Test
+    public void testUserRegister() {
+        when(userService.createUser(any(CreateUser.class))).thenReturn(1L);
+
+        UserCreated userCreated = userController.userRegister(
+                new CreateUser("any_email@mail.com",
+                        "any_password",
+                        "any_password",
+                        "any_name",
+                        LocalDate.now()));
+        assertNotNull(userCreated);
+        assertEquals(1L, userCreated.getId());
     }
 }
