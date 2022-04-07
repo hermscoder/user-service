fcf209792f783be413e7ddb5e46027472a6671df
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-04-07 07:45:34 +0100

    chore: changing dbSeeder to fire only when db update

--- data/src/main/java/com/shareit/data/repository/DbSeeder.java
+++ data/src/main/java/com/shareit/data/repository/DbSeeder.java
@@ -1,21 +1,30 @@
 package com.shareit.data.repository;
 
 import com.shareit.domain.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 
 @Component
+@Profile({"test", "dev"})
 public class DbSeeder implements CommandLineRunner {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
-    public DbSeeder(UserRepository userRepository) {
+    private final String strategy;
+
+    public DbSeeder(UserRepository userRepository, @Value(value = "${spring.jpa.hibernate.ddl-auto:none}") String strategy) {
         this.userRepository = userRepository;
+        this.strategy = strategy;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        userRepository.save(new User(null, "test@gmail.com", "password", "test", LocalDate.now()));
+        if(!"create".equals(strategy)) {
+            return;
+        }
+        userRepository.save(new User("test@gmail.com", "password", "test", LocalDate.now()));
     }
 }
