8f6cdf6e2f2555362c6e6064bd487e2b834cd19b
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-04-09 19:13:07 +0100

    refactor: removing unnecessary imports

--- presentation/src/test/java/com/shareit/presentation/UserControllerTest.java
+++ presentation/src/test/java/com/shareit/presentation/UserControllerTest.java
@@ -8,8 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.time.LocalDate;
-import java.util.Collections;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
