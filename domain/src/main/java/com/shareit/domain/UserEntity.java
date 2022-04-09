4ecc7cfbf5e7d701246003ff4999a6cadcb36d25
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-04-09 15:56:27 +0100

    refactor: replacing all the written code by the lombok annotations

--- domain/src/main/java/com/shareit/domain/UserEntity.java
+++ domain/src/main/java/com/shareit/domain/UserEntity.java
@@ -1,11 +1,17 @@
 package com.shareit.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.Objects;
 
 @Entity
 @Table(name = "s_user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
@@ -21,79 +27,4 @@ public class UserEntity {
         this(null,email, password, name, birthDate);
     }
 
-    public UserEntity(Long id, String email, String password, String name, LocalDate birthDate) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.name = name;
-        this.birthDate = birthDate;
-    }
-
-    public UserEntity() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        UserEntity userEntity = (UserEntity) o;
-        return Objects.equals(id, userEntity.id) && Objects.equals(email, userEntity.email) && Objects.equals(password, userEntity.password) && Objects.equals(name, userEntity.name) && Objects.equals(birthDate, userEntity.birthDate);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, email, password, name, birthDate);
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", name='" + name + '\'' +
-                ", birthDate=" + birthDate +
-                '}';
-    }
 }
--- domain/src/main/java/com/shareit/domain/dto/User.java
+++ domain/src/main/java/com/shareit/domain/dto/User.java
@@ -4,10 +4,15 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.shareit.utils.deserializer.LocalDateDeserializer;
 import com.shareit.utils.serializer.LocalDateSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.Objects;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     private Long id;
@@ -23,70 +28,5 @@ public class User {
 
     public User(String email, String password, String name, LocalDate birthDate) {
         this(null,email, password, name, birthDate);
-    }
-
-    public User(Long id, String email, String password, String name, LocalDate birthDate) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.name = name;
-        this.birthDate = birthDate;
-    }
-
-    public User() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(name, user.name) && Objects.equals(birthDate, user.birthDate);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, email, password, name, birthDate);
     }
 }
