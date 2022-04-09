a44b59e36a62d1c0df5847bc89dec6f444180da0
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-04-09 14:43:17 +0100

    feat: using the serializer and deserializer on the DTO and Model.

--- domain/src/main/java/com/shareit/domain/dto/User.java
+++ domain/src/main/java/com/shareit/domain/dto/User.java
@@ -2,8 +2,8 @@ package com.shareit.domain.dto;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.shareit.utils.deserializer.LocalDateDeserializer;
+import com.shareit.utils.serializer.LocalDateSerializer;
 
 import java.time.LocalDate;
 import java.util.Objects;
