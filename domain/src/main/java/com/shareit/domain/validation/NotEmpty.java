c2277cc516eac8c469f62f70813438bab64f0211
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-04-09 19:11:01 +0100

    fix: fixing annotation message

--- domain/src/main/java/com/shareit/domain/validation/NotEmpty.java
+++ domain/src/main/java/com/shareit/domain/validation/NotEmpty.java
@@ -10,7 +10,7 @@ import java.lang.annotation.*;
 @Constraint(validatedBy = NotEmptyValidator.class)
 public @interface NotEmpty {
     //error message
-    public String message() default "Missing param: $1";
+    public String message() default "Missing param: %s";
     //represents group of constraints
     public Class<?>[] groups() default {};
     //represents additional information about annotation
