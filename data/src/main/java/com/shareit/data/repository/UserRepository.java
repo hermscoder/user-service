b47b56f970efc3d08a5f4f4f753b159614545867
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-04-15 09:47:22 +0100

    feat: adding method to change user state

--- data/src/main/java/com/shareit/data/repository/UserRepository.java
+++ data/src/main/java/com/shareit/data/repository/UserRepository.java
@@ -1,9 +1,19 @@
 package com.shareit.data.repository;
 
 import com.shareit.domain.entity.UserEntity;
+import com.shareit.domain.entity.UserState;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    @Transactional
+    @Modifying
+    @Query("UPDATE UserEntity a " +
+            "SET a.state = ?1  WHERE a.email = ?2")
+    int changeUserState(UserState confirmed, String email);
 }
