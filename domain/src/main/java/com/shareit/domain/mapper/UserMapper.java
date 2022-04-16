6eba0063decddc3dddd068610af975f2467ea791
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-04-16 19:12:14 +0100

    fix: fixing mapper to fill confirmed field in the DTO based on the state.

--- domain/src/main/java/com/shareit/domain/mapper/UserMapper.java
+++ domain/src/main/java/com/shareit/domain/mapper/UserMapper.java
@@ -2,7 +2,10 @@ package com.shareit.domain.mapper;
 
 import com.shareit.domain.entity.UserEntity;
 import com.shareit.domain.dto.User;
+import com.shareit.domain.entity.UserState;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +14,11 @@ import org.springframework.stereotype.Component;
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
+    @Mapping(target = "confirmed", source = "state", qualifiedByName = "isUserConfirmed")
     User toModel(UserEntity userEntity);
-    UserEntity toEntity(User model);
 
+    @Named("isUserConfirmed")
+    default Boolean isUserConfirmed(UserState userState) {
+        return UserState.CONFIRMED.equals(userState);
+    }
 }
