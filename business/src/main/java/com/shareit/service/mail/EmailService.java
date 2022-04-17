1bda541f3f2645d4ee2c70ee98aeed8c655b5190
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-04-17 23:03:57 +0100

    refactor: removing unnused imports

--- business/src/main/java/com/shareit/service/mail/EmailService.java
+++ business/src/main/java/com/shareit/service/mail/EmailService.java
@@ -7,8 +7,6 @@ import com.shareit.utils.commons.exception.EmailSenderException;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
