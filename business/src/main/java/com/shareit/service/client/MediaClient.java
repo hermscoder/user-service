a24042f6fa8d6e76f525da0c6f47b7eaf2c72737
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-05-04 19:44:41 +0100

    feature: adding MediaClient interface and v1 implementation to meet the independent deployability principle

---
+++ business/src/main/java/com/shareit/service/client/MediaClient.java
@@ -0,0 +1,8 @@
+package com.shareit.service.client;
+
+import com.shareit.domain.dto.Media;
+
+public interface MediaClient {
+
+    Media getMediaById(Long mediaId);
+}
---
+++ business/src/main/java/com/shareit/service/client/MediaClientV1.java
@@ -0,0 +1,24 @@
+package com.shareit.service.client;
+
+import com.shareit.domain.dto.Media;
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class MediaClientV1 implements MediaClient {
+
+    private final WebClient webClient;
+
+    public MediaClientV1(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public Media getMediaById(Long mediaId) {
+        Media userMedia = webClient.get()
+                .uri("http://localhost:8082/v1/media",
+                        uriBuilder -> uriBuilder.build(mediaId))
+                .retrieve()
+                .bodyToMono(Media.class)
+                .block();
+
+        return userMedia;
+    }
+}
