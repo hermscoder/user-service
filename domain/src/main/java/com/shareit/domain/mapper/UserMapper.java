0808560f3626b3ac0a23d4bf1418c98af147ed4e
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-05-09 19:57:25 +0100

    feature: adding media to User model map

--- domain/src/main/java/com/shareit/domain/mapper/UserMapper.java
+++ domain/src/main/java/com/shareit/domain/mapper/UserMapper.java
@@ -1,5 +1,7 @@
 package com.shareit.domain.mapper;
 
+import com.shareit.domain.dto.Media;
+import com.shareit.domain.dto.registration.UserRegistration;
 import com.shareit.domain.entity.UserEntity;
 import com.shareit.domain.dto.User;
 import com.shareit.domain.entity.UserState;
@@ -14,8 +16,14 @@ import org.springframework.stereotype.Component;
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
-    @Mapping(target = "confirmed", source = "state", qualifiedByName = "isUserConfirmed")
-    User toModel(UserEntity userEntity);
+    @Mapping(target = "id", source = "entity.id")
+    @Mapping(target = "name", source = "entity.name")
+    @Mapping(target = "birthDate", source = "entity.birthDate")
+    @Mapping(target = "confirmed", source = "entity.state", qualifiedByName = "isUserConfirmed")
+    @Mapping(target = "profilePicture", source = "profilePicture")
+    User toModel(UserEntity entity, Media profilePicture);
+
+    UserEntity toEntity(UserRegistration userRegistration);
 
     @Named("isUserConfirmed")
     default Boolean isUserConfirmed(UserState userState) {
