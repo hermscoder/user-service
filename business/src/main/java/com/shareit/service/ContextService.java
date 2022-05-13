f486d79849044a84cd4ed452d74d3ae5f28e9d3c
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-05-13 08:35:43 +0100

    fix: adding the media featch to the currentUser mock

--- business/src/main/java/com/shareit/service/ContextService.java
+++ business/src/main/java/com/shareit/service/ContextService.java
@@ -2,20 +2,25 @@ package com.shareit.service;
 
 import com.shareit.data.repository.UserRepository;
 import com.shareit.domain.dto.User;
+import com.shareit.domain.entity.UserEntity;
 import com.shareit.domain.mapper.UserMapper;
+import com.shareit.service.client.MediaClient;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ContextService implements AuthService {
     private final UserRepository userRepository;
+    private final MediaClient mediaClient;
 
-    public ContextService(UserRepository userRepository) {
+    public ContextService(UserRepository userRepository, MediaClient mediaClient) {
         this.userRepository = userRepository;
+        this.mediaClient = mediaClient;
     }
 
     //TODO get the REAL current user, from the token.
     @Override
     public User getCurrentUser() {
-        return UserMapper.INSTANCE.toModel(userRepository.getById(1L), null);
+        UserEntity userEntity = userRepository.getById(1L);
+        return UserMapper.INSTANCE.toModel(userEntity, mediaClient.getMediaById(userEntity.getProfilePictureId()));
     }
 }
