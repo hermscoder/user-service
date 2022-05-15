4ec2c5e95736431eb136d1bc3c293a49fa13215c
Emerson Ribeiro <emersonrjr03@gmail.com>
2022-05-15 15:40:17 +0100

    refactor: removing unnecessary comments

--- business/src/main/java/com/shareit/service/registration/ConfirmationTokenService.java
+++ business/src/main/java/com/shareit/service/registration/ConfirmationTokenService.java
@@ -67,7 +67,6 @@ public class ConfirmationTokenService {
                 .whenCompleteAsync((res, ex) -> {
                     if (ex != null) {
                         LOGGER.error(ex.getMessage(), ex);
-                        //TODO add logic to email send retry
                     }
                 });
     }
